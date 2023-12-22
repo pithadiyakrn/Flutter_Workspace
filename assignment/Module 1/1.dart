e08bda64cb9b4dbe4cdb231661ae45cd91fb6977
pithadiyakrn <84989958+pithadiyakrn@users.noreply.github.com>
2023-12-22 16:37:09 +0530

    Add files via upload

---
+++ assignment/Module 1/1.dart
@@ -0,0 +1,12 @@
+import 'dart:io';
+
+void main()
+{
+  print("My Name is Bansi Kansagara");
+
+  print("My BirthDate is 16'April,2002");
+
+  print("I am 21 years old");
+
+  print("I live in Rajkot");
+}
